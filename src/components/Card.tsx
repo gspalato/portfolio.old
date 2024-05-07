b3fb74547da4049cdd6648aef49143f362f3d37b
Gabriel <3950465+gspalato@users.noreply.github.com>
2024-05-07 14:37:09 -0300

    New card component, with mouse glow on background and slight perspective effect.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,90 +1,46 @@
-import { motion, useMotionValue, useSpring, useTransform } from 'framer-motion';
+import { motion, useMotionTemplate, useMotionValue } from 'framer-motion';
 import React from 'react';
 
-import classes from '@/lib/classes';
+import classes from '@lib/classes';
 
 type Props = {
 	className?: string;
-	style?: React.CSSProperties;
-
-	tiltOnHover?: boolean;
-	hoverTiltDegreesX?: number;
-	hoverTiltDegreesY?: number;
 } & React.PropsWithChildren;
 
 const Component: React.FC<Props> = (props) => {
-	const {
-		children,
-		className,
-		hoverTiltDegreesX = 7.5,
-		hoverTiltDegreesY = 7.5,
-		tiltOnHover = true,
-		style,
-	} = props;
+	const { children, className } = props;
 
-	const classNames = classes(
-		'bg-overlay-1 border-overlay-2 relative h-72 w-96 rounded-md border backdrop-blur-md',
-		'transition-shadow duration-300 hover:shadow-lg',
-		'bg-gradient-to-tl from-black to-[#111]',
+	const mouseX = useMotionValue(0);
+	const mouseY = useMotionValue(0);
+
+	const containerClassnames = classes(
+		'group relative w-full max-w-[350px] overflow-hidden rounded-xl',
 		className,
 	);
-
-	const x = useMotionValue(0);
-	const y = useMotionValue(0);
-
-	const mouseXSpring = useSpring(x);
-	const mouseYSpring = useSpring(y);
-
-	const rotateX = useTransform(
-		mouseYSpring,
-		[-0.5, 0.5],
-		[`${hoverTiltDegreesX}deg`, `-${hoverTiltDegreesX}deg`],
-	);
-	const rotateY = useTransform(
-		mouseXSpring,
-		[-0.5, 0.5],
-		[`-${hoverTiltDegreesY}deg`, `${hoverTiltDegreesY}deg`],
-	);
-
-	const onMouseOver = (e: React.MouseEvent<HTMLDivElement>) => {
-		const element = e.currentTarget;
-		const rect = element.getBoundingClientRect();
-
-		const width = rect.width;
-		const height = rect.height;
-
-		const mouseX = e.clientX - rect.left;
-		const mouseY = e.clientY - rect.top;
-
-		const xPercentage = mouseX / width - 0.5;
-		const yPercentage = mouseY / height - 0.5;
-
-		x.set(xPercentage);
-		y.set(yPercentage);
-	};
-
-	const onMouseLeave = () => {
-		x.set(0);
-		y.set(0);
-	};
 
 	return (
-		<motion.div
-			className={classNames}
-			style={{
-				transformStyle: 'preserve-3d',
-				rotateX: tiltOnHover ? rotateX : undefined,
-				rotateY: tiltOnHover ? rotateY : undefined,
-				...style,
+		<div
+			onMouseMove={(e) => {
+				const { left, top } = e.currentTarget.getBoundingClientRect();
+
+				mouseX.set(e.clientX - left);
+				mouseY.set(e.clientY - top);
 			}}
-			whileHover={{
-				translateZ: -20,
-			}}
-			onMouseOver={onMouseOver}
-			onMouseLeave={onMouseLeave}
+			className={containerClassnames}
 		>
-			{children}
-		</motion.div>
+			<motion.div className='absolute top-0 right-0 h-px w-80 bg-gradient-to-l from-transparent via-white/30 via-10% to-transparent' />
+			<motion.div
+				className='pointer-events-none absolute -inset-px rounded-xl opacity-0 transition duration-300 group-hover:opacity-100'
+				style={{
+					background: useMotionTemplate`
+						radial-gradient(200px circle at ${mouseX}px ${mouseY}px, rgba(38, 38, 38, 0.4), transparent 80%)
+					`,
+				}}
+			/>
+			<div className='relative flex flex-col gap-3 rounded-xl border border-white/10 py-5 px-4'>
+				{children}
+			</div>
+		</div>
 	);
 };
 
