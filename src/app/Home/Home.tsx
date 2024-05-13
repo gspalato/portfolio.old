72b77a408972d54731469f6db4ab0a924aae91f5
Gabriel <3950465+gspalato@users.noreply.github.com>
2024-05-13 09:38:50 -0300

    Home page now fetches projects from API. using Project Context.

--- src/app/Home/Home.tsx
+++ src/app/Home/Home.tsx
@@ -1,4 +1,5 @@
 import { AnimatePresence, motion } from 'framer-motion';
+import { useEffect } from 'react';
 import { LiaArrowDownSolid } from 'react-icons/lia';
 import { Masonry } from 'react-plock';
 
@@ -8,35 +9,25 @@ import PageWrapper from '@components/PageWrapper';
 
 import classes from '@lib/classes';
 
+import { useProjects } from '@/lib/api/projects';
+
 import ProjectCard from './components/ProjectCard';
 import Styles from './Home.module.css';
-
-const Projects = [
-	{
-		title: 'Ecobucks',
-		description:
-			'A concept project for a proper waste disposal reward system.',
-		img: 'https://i.ibb.co/bBKN3NP/i-Phone-Home-Mockup-2.jpg',
-		to: 'https://github.com/gspalato/ecobucks',
-	},
-	{
-		title: 'Ecobucks Station Firmware',
-		description: "The firmware for Ecobucks' waste disposal stations.",
-		img: 'https://i.ibb.co/QvdKfb0/jpeg-optimizer-Gradient-Firmware-Banner-for-Portfolio.jpg',
-		to: 'https://github.com/gspalato/ecobucks_station_firmware',
-	},
-	{
-		title: 'Portfolio',
-		description: "My personal portfolio website. You're looking at it!",
-		img: 'https://i.ibb.co/HGbSYfY/Portfolio-Banner.jpg',
-		to: 'https://github.com/gspalato/portfolio',
-	},
-];
 
 const AnimatedBadge = motion(Badge);
 const AnimatedProjectCard = motion(ProjectCard);
 
 const Page = () => {
+	const { fetchProjects, projects, success, loading } = useProjects();
+
+	useEffect(() => {
+		fetchProjects();
+	}, []);
+
+	useEffect(() => {
+		console.log(projects);
+	}, [projects]);
+
 	const heroTextClassnamesMobile = classes(
 		'text-glow font-title text-center text-[17.5vw] font-bold leading-[17.5vw] tracking-tight text-[#eee] sm:hidden',
 		Styles.hero_text_glow,
@@ -45,6 +36,11 @@ const Page = () => {
 	const heroTextClassnamesDesktop = classes(
 		'text-glow font-title hidden text-center text-[10vw] font-bold leading-[10vw] tracking-tight text-[#eee] sm:block md:text-[90px] md:leading-[90px]',
 		Styles.hero_text_glow,
+	);
+
+	const projectsSectionClassnames = classes(
+		'container flex w-full flex-col gap-20 pt-20 px-8 lg:w-5xl lg:px-0',
+		success ? 'min-h-screen' : 'pb-20',
 	);
 
 	return (
@@ -88,35 +84,49 @@ const Page = () => {
 				/>
 			</section>
 			<Divisor />
-			<section className='container flex min-h-screen w-full flex-col gap-20 pt-20 px-8 lg:w-5xl lg:px-0'>
+			<section className={projectsSectionClassnames}>
 				<h1 className='font-title text-text text-center text-5xl'>
 					Projects
 				</h1>
-				<Masonry
-					items={Projects}
-					config={{
-						columns: [1, 2],
-						gap: [24, 12],
-						media: [640, 768],
-					}}
-					render={(item, idx) => (
-						<AnimatedProjectCard
-							key={idx}
-							containerClassName='min-w-[40%]'
-							className='hover:bg-overlay-1 group hover:border-overlay-1 flex min-h-[400px] flex-col gap-3 border-none transition-all transition-all duration-300 duration-300 hover:scale-101 hover:cursor-pointer'
-							initial={{ opacity: 0 }}
-							whileInView={{ opacity: 1 }}
-							transition={{
-								type: 'spring',
-								ease: 'easeInOut',
-								duration: 0.35,
-							}}
-							img={item.img}
-							title={item.title}
-							description={item.description}
-						/>
-					)}
-				/>
+				{success && (
+					<Masonry
+						items={projects}
+						config={{
+							columns: [1, 2],
+							gap: [24, 12],
+							media: [640, 768],
+						}}
+						render={(item, idx) => (
+							<AnimatedProjectCard
+								key={idx}
+								containerClassName='min-w-[40%]'
+								className='hover:bg-overlay-1 group hover:border-overlay-1 flex min-h-[400px] flex-col gap-3 border-none transition-all transition-all duration-300 duration-300 hover:scale-101 hover:cursor-pointer'
+								initial={{ opacity: 0 }}
+								whileInView={{ opacity: 1 }}
+								transition={{
+									type: 'spring',
+									ease: 'easeInOut',
+									duration: 0.35,
+								}}
+								img={item.banner_url}
+								title={item.name}
+								description={item.description}
+							/>
+						)}
+					/>
+				)}
+				{loading && (
+					<div className='flex h-full items-center justify-center'>
+						<div className='border-overlay-1 h-16 w-16 animate-spin rounded-full border-b-4' />
+					</div>
+				)}
+				{!success && !loading && (
+					<div className='flex h-full items-center justify-center'>
+						<h1 className='font-body text-text-tertiary text-md'>
+							No projects found.
+						</h1>
+					</div>
+				)}
 			</section>
 		</PageWrapper>
 	);
