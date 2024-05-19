0b655716f7e0cc3e21a45a7a014f3fe9a78de4e9
Gabriel <3950465+gspalato@users.noreply.github.com>
2024-05-19 18:45:26 -0300

    Small changes to the About page.

--- src/app/About/About.tsx
+++ src/app/About/About.tsx
@@ -7,7 +7,7 @@ const Page = () => {
 		<PageWrapper className='flex items-center'>
 			<section className='container relative flex h-screen flex-col items-center justify-center gap-4 gap-6 lg:w-6xl'>
 				<h1 className='font-title text-text text-center text-6xl'>
-					About Me
+					About
 				</h1>
 				<h2 className='font-body text-text-secondary text-center text-sm tracking-wide'>
 					I'm working on this page. Check back later!
